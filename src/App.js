95493c738c3a8c15b98bd96d05ce414618f31258
VISHNUVARTHAN U <vvarthan7@gmail.com>
2022-07-30 21:29:23 +0530

    API URL update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from "./MovieCard";
 
-const API_URL = 'http://www.omdbapi.com/?apikey=6ee02946';
+const API_URL = 'https://www.omdbapi.com/?apikey=6ee02946';
 
 function App() {
   const [movies, setMovies] = useState([]);
